bd796c809c2ba86fab5bfc4e229039dde7f635a0
KriuchkovSP <kryuchkovsp@mail.ru>
2022-06-23 14:15:44 +0500

    Update Task 05

--- sem0/Task_05/Program.cs
+++ sem0/Task_05/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает
+// все целые числа в промежутке от -N до N.
+//  4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Программа запрашивает целое число N выводит диапазон чисел от -N до N");
+Console.Write("Введите целое число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = -n;
+Console.WriteLine("Полученный диапазон чисел от -N до N:");
+while (i < n)
+{
+    Console.Write($"{i}, ");
+    i++;
+}
+Console.WriteLine(n);
