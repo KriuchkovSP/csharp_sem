bb5323c51dad8c488582fd59311508c4858b495b
KriuchkovSP <kryuchkovsp@mail.ru>
2022-06-24 16:44:02 +0500

    Complete Task 08

--- sem0/Task_08/Program.cs
+++ sem0/Task_08/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Программа запрашивает целое положительное число N выводит все четные числа от 1 до N");
+Console.Write("Введите целое положительное число больше 0: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 1)
+{
+    Console.WriteLine("Полученные четные числа от 1 до N: ");
+    int i = 2;
+    while (i < n - 1)
+    {
+        Console.Write($"{i}, ");
+        i += 2;
+    }
+    if ((n % 2) == 0) Console.WriteLine(i);
+    else Console.WriteLine(i);
+}
+else Console.WriteLine("Число <= 0");
