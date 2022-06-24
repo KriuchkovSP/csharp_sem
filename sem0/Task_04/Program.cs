f4a5791abc5eacee1d3f03281b2218203d182e8b
KriuchkovSP <kryuchkovsp@mail.ru>
2022-06-24 16:05:01 +0500

    Complete Task 04

--- sem0/Task_04/Program.cs
+++ sem0/Task_04/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int i = 0;
+int[] nummas = new int[3];
+int max_pos = 0;
+Console.WriteLine("Программа запрашивает 3 целых числа и выдает максимальное из них");
+while (i < 3)
+{
+    Console.Write($"Введите {i + 1} целое число: ");
+    nummas[i] = Convert.ToInt32(Console.ReadLine());
+    if (nummas[max_pos] < nummas[i]) max_pos = i;
+    i++;
+}
+
+Console.WriteLine($"Введенное максимальное число: {nummas[max_pos]}");
