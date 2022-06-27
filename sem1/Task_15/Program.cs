84c92922b980620506a8c027f28cbf1531be1155
KriuchkovSP <kryuchkovsp@mail.ru>
2022-06-27 16:20:31 +0500

    Complete Task 15

--- sem1/Task_15/Program.cs
+++ sem1/Task_15/Program.cs
@@ -5,3 +5,15 @@
 // 7 -> да
 // 1 -> нет
 
+Console.WriteLine("Программа запрашивает целое число и выводит соответствующий день недели");
+Console.Write("Введите целое число от 1 до 7: ");
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+string DayWeek (int day)
+{
+    if (day < 1 || day > 7) return $"Введен некорректный день недели {day}";
+    if (day == 6 || day == 7) return $"Введеный день {day} выходной";
+    return $"Введеный день {day} не является выходным";
+}
+
+Console.WriteLine(DayWeek(dayWeek));
