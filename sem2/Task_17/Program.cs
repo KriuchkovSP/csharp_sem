c1cfb9bc76593565d5edc80222b62725d8a80ee1
Sergei Kriuchkov <kryuchkovsp@mail.ru>
2022-06-30 19:30:58 +0500

    Update task 17

--- sem2/Task_17/Program.cs
+++ sem2/Task_17/Program.cs
@@ -2,14 +2,14 @@
 // координаты точки (Х и Y), причем X != 0 и Y != 0 и выдает
 // номер четверти плоскости, в которой находится эта точка.
 
-Console.WriteLine ("Введите координаты точки (X и Y)");
-Console.Write ("X: ");
-int x = Convert.ToInt32 (Console.ReadLine());
-Console.Write ("Y: ");
-int y = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine ("Введите координаты точки (X и Y)");
+// Console.Write ("X: ");
+// int x = Convert.ToInt32 (Console.ReadLine());
+// Console.Write ("Y: ");
+// int y = Convert.ToInt32 (Console.ReadLine());
 
-
-
+Console.Write ("Введите координаты точки (X и Y) через пробел:");
+int[] xy = Console.ReadLine().Split().Select(int.Parse).ToArray(); // Split() Если пробел, то пусто, если знак, то 'знак'
 
 string Quarter (int xc, int yc)
 {
@@ -19,6 +19,6 @@ string Quarter (int xc, int yc)
     if (xc > 0 && yc < 0) return "Четвертая четверть";
     return "Введены некорректные координаты";
 }
-string result = Quarter(x, y);
+string result = Quarter(xy[0], xy[1]);
 
 Console.WriteLine (result);
