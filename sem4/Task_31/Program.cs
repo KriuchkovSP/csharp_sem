4bfcb516fe0fb6cdd1312de4cfc875ba70671fdf
KriuchkovSP <kryuchkovsp@mail.ru>
2022-07-07 13:26:13 +0500

    Create sem4 and Tasks 31-33

---
+++ sem4/Task_31/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6]
+// сумма положительных чисел равна 29,
+// сумма отрицательных равна -20.
+
+int[] CreateArrayRndInt (int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+int[] GetSumNegPosElem (int[] array)
+{
+    int sumNeg = 0;
+    int sumPos = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0) sumNeg += array[i];
+        else sumPos += array[i];
+    }
+    return new[] {sumPos,sumNeg};
+}
+
+void PrintArr(int[] array)
+{
+    Console.WriteLine ($"Сгенерированный массив из {array.Length} элементов:");
+    for (int i = 0; i < array.Length;i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write ($"{array[i]}, ");
+        else Console.WriteLine ($"{array[i]}]");
+    }
+}
+
+void PrintSumNegPosArr(int[] array)
+{
+    Console.WriteLine($"Cумма положительных чисел равна {array[0]}");
+    Console.WriteLine($"Cумма отрицательных равна {array[1]}");
+}
+
+Console.Write ("Введите размер массива: ");
+//int insize = 12;
+int insize = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите минимальное число диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите максимальное число диапазона: ");
+int max =  Convert.ToInt32(Console.ReadLine ());
+
+int[] genarr = CreateArrayRndInt(insize, min, max);
+int[] sumNegPosElem = GetSumNegPosElem(genarr);
+
+PrintArr(genarr);
+PrintSumNegPosArr(sumNegPosElem);
---
+++ sem4/Task_33/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да 
+
+int[] CreateArrayRndInt (int size, int begin, int end)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+void PrintArr(int[] array)
+{
+    Console.WriteLine ($"Сгенерированный массив из {array.Length} элементов:");
+    for (int i = 0; i < array.Length;i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write ($"{array[i]}, ");
+        else Console.WriteLine ($"{array[i]}]");
+    }
+}
+
+bool SearchElem (int[] array, int elem)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == elem) return true;
+    }
+    return false;
+}
+
+void PrintRes (bool resSearch)
+{
+    string res = resSearch ? "Искомый элемент присутствует в массиве" : "Искомый элемент отсутствует в массиве";
+    Console.WriteLine(res);
+}
+
+// Console.Write ("Введите размер массива: ");
+// int insize = 12;
+// int insize = Convert.ToInt32(Console.ReadLine ());
+// Console.Write ("Введите минимальное число диапазона: ");
+// int min = Convert.ToInt32(Console.ReadLine ());
+// Console.Write ("Введите максимальное число диапазона: ");
+// int max =  Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите значение для поиска: ");
+int elem =  Convert.ToInt32(Console.ReadLine ());
+
+
+//int[] genarr = CreateArrayRndInt(insize, min, max);
+int[] arr = {6, 7, 19, 345, 3};
+PrintArr(arr);
+bool resultSearch = SearchElem(arr, elem);
+PrintRes(resultSearch);
